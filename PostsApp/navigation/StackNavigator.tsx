89195c8d93e66c666d105e5494b1599787e5e604
Maryna-Korbet <m22v86k@gmail.com>
2024-12-23 21:28:25 -0500

    updated .idea

--- PostsApp/navigation/StackNavigator.tsx
+++ PostsApp/navigation/StackNavigator.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useSelector } from 'react-redux';
 
@@ -22,7 +23,7 @@ const StackNavigator = () => {
     
     return (
         <Stack.Navigator
-            initialRouteName="Login"
+        /* initialRouteName="Login" */
         >
             {!user ? (
                 //if user is not logged in go to Login
